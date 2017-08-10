a0bd5c7e6e0b01e081f497c24ba338e627319d3d
indirectlylit <devon@indirectlylit.com>
2017-08-10 09:39:01 -0700

    add URL

--- src/Start.vue
+++ src/Start.vue
@@ -2,6 +2,7 @@
 
   <div>
     <h1>the fantastic hash race</h1>
+    <h2>or, not exactly the best use of valuable time two days before going on vacation...</h2>
     <p>are you ready to race?</p>
     <p>
       <button v-if="yes" @click="yes = false" class="pure-button pure-button-primary">
@@ -38,4 +39,10 @@
 </script>
 
 
-<style scoped></style>
+<style scoped>
+
+  h2 {
+    color: white;
+  }
+
+</style>
