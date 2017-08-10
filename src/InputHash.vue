d6fa71a833bce4fd92f14cb1979c226e04e6cb57
indirectlylit <devon@indirectlylit.com>
2017-08-10 13:02:16 -0700

    autofocus field

--- src/InputHash.vue
+++ src/InputHash.vue
@@ -4,7 +4,7 @@
     <form @submit.prevent="NEXT" class="pure-form pure-form-stacked">
       <fieldset>
         <legend>What is it?</legend>
-        <input v-model="input" type="text" placeholder="(the hash)" autofocus>
+        <input v-model="input" type="text" placeholder="(the hash)" autofocus ref="input">
         <button type="submit" class="pure-button pure-button-primary" :disabled="input === ''">NEXT!</button>
       </fieldset>
     </form>
@@ -26,6 +26,7 @@
       NEXT() {
         this.$emit('next', this.input);
         this.input = "";
+        this.$refs.input.focus();
       },
     },
   }
