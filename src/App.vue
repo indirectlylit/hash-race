f6ad7780de89e28b1f88b88db6318b23e69c6111
indirectlylit <devon@indirectlylit.com>
2017-08-10 12:31:02 -0700

    add race length parameter

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <Race
       v-if="state === 'race'"
       @done="showResults"
-      :raceLength="600"
+      :raceLength="raceLength"
     />
     <Finish v-if="state === 'finish'" :log="log" />
   </div>
@@ -17,6 +17,18 @@
   import Start from './Start.vue';
   import Race from './Race.vue';
   import Finish from './Finish.vue';
+
+
+  // https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+  function getParameter(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 
   export default {
     name: 'app',
@@ -32,10 +44,17 @@
         this.state = 'finish';
       },
     },
-    data () {
+    data() {
       return {
         state: 'start',
         log: [],
+        raceLength: 600,
+      }
+    },
+    mounted() {
+      const raceLength = getParameter('length', window.location.href);
+      if (parseInt(raceLength)) {
+        this.raceLength = parseInt(raceLength);
       }
     }
   }
